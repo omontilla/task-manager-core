59b80f6ae304e5358d4beb3a1d78f733bddee023
Oscar Montilla <omontilla@unow.es>
2024-12-29 00:19:38 -0400

    conexion

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,19 @@
+import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: process.env.MONGO_URL,
+        user: process.env.MONGOUSER,
+        pass: process.env.MONGOPASSWORD,
+      }),
+      inject: [ConfigService],
+    }),
   ],
   exports: [MongooseModule],
 })
